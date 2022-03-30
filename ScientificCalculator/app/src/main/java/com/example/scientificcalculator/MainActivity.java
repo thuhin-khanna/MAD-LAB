120a3351feeb372ca43d6bcac83ecbbfe408573c
thuhinkhanna <qor6119@gmail.com>
2022-03-30 22:46:17 +0530

    Scientific Calculator

---
+++ ScientificCalculator/app/src/main/java/com/example/scientificcalculator/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.scientificcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //+, *, /, =, cos, sin, tan, pow, sqrt, log, lan and mod
+
+    }
+}
